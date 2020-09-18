e55f28e19c54b53d880f4e1dabe4d5450ee431ef
hal0998 <halmuratshool@gmail.com>
2020-09-18 11:13:47 -0400

    Git-Api-Automation

---
+++ GitHub_API_Automation/src/main/java/GitHub_API_Automation/GitHub_API_Automation/App.java
@@ -0,0 +1,13 @@
+package GitHub_API_Automation.GitHub_API_Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
